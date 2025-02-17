dcdb0eac06d4726c0f45c80dfd5a191d79eff57a
Md. Abu Nayem <abunayem119@gmail.com>
2025-02-18 00:16:55 +0600

    Update firebase.init.js

--- src/auth/firebase.init.js
+++ src/auth/firebase.init.js
@@ -5,14 +5,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCGjKGBuX9r0HZt-gSw83-d14uU4GXrA5I",
-  authDomain: "game-revew-app.firebaseapp.com",
-  projectId: "game-revew-app",
-  storageBucket: "game-revew-app.firebasestorage.app",
-  messagingSenderId: "184989141270",
-  appId: "1:184989141270:web:d86afeabb3c5dd25f26837"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
